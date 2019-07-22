ca7e9bef39bb8a8ee767b03ab4f93b46d1987615
Jm <Jean.M.Girard@Outlook.com>
2019-07-22 00:49:15 -0400

    ts

--- gulp-less/gulpfile.js
+++ gulp-less/gulpfile.js
@@ -2,42 +2,90 @@
  *  Imports requirements
  */
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    concat = require('gulp-concat'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    path = require('path'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps');
 
 const { series, parallel, task } = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
 
-/** 
+
+
+
+/**
  * Defining Tasks
+ *
+ * Less
  */
-
-function build_less(){
-    return gulp.src('./less/**/*.less')
-        .pipe(less({
-            paths: [ "./less/**/*.less" ]})
-        )
-        .pipe(gulp.dest('./dist/css'));
-}
 task('less', function() {
-    return build_less()
-});
-task('watch-less', function() {
-    return watch('./less/**/*.less', { ignoreInitial: false })
-        .pipe(less({
-            paths: [ "./less/**/*.less" ]})
-        )
+    return gulp.src('./src/less/app.less')
+        .pipe(less())
         .pipe(gulp.dest('./dist/css'));
 });
 
+task('build', parallel('less'));
+task('dev', series('less-watch'));
 /** 
- * Defining default task (>> gulp)
+ * default task 
+ * > gulp
  */
-
 
 function defaultTask(cb) {
-    // place code for your default task here
+    // place code here
     cb();
 }  
-
 exports.default = defaultTask
+
+
+
+
+
+
+
+/**
+ *  MORE EXEMPLES
+ */
+
+ /**
+  * WATCH
+  */
+task('less-watch', function() {
+    return watch('./src/less/**/*.less', { ignoreInitial: false })
+        .pipe(less())
+        .pipe(gulp.dest('./dist/css'));
+});
+/**
+ *  CONCAT - (Important que concat soit avant less)
+ */
+ task('less-concat', function(){
+    return gulp.src('./src/less/**/*.less')
+    .pipe(concat("style.less"))
+    .pipe(less())
+    .pipe(cleanCSS({debug: true, compatibility: 'ie8'}, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}Kb`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}Kb`);
+    }))
+    .pipe(gulp.dest('./dist/concat'));
+ });
+
+ /**
+  * SOURCEMAPS & MINIFY
+  */
+ task('minify-css',() => {
+    return gulp.src('./src/*.css')
+      .pipe(sourcemaps.init())
+      .pipe(cleanCSS())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./dist/minify'));
+});
+
+
+ task('sourcemaps-css', function(){
+    return gulp.src('./src/less/**/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/sourcemaps'));
+ });

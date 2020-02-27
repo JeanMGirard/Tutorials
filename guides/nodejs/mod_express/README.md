6403af716190276d86ec1bee918f2b722a4b5539
Jm <Jean.M.Girard@Outlook.com>
2020-02-27 05:56:51 -0500

    articles

--- guides/nodejs/mod_express/README.md
+++ guides/nodejs/mod_express/README.md
@@ -2,3 +2,7 @@
 
 [app](/nodejs/mod_express/app.js ':include :type=code')
 [app](/nodejs/mod_express/app.js ':include :type=code :fragment=view-engine')
+
+
+
+[app](app.js ':include :type=code :fragment=view-engine')
--- guides/nodejs/mod_express/routes/users.js
+++ guides/nodejs/mod_express/routes/users.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var matteoRouter = require("./matteo");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.use("/matteo", matteoRouter);
 
 module.exports = router;
